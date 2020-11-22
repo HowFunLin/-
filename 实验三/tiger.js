377861e02ea24f8aae01227bec40ca366d46f7e3
HowFunLin <1003941051@qq.com>
2020-11-22 13:37:28 +0800

    完成鼠标悬停替换图片
    Signed-off-by: HowFunLin <1003941051@qq.com>

---
+++ 实验三/tiger.js
@@ -0,0 +1,14 @@
+window.onload = function () 
+{
+    var img = document.getElementById("tiger");
+    
+    img.onmouseover = function ()
+    {
+        this.src = "img/tiger.png";
+    }
+
+    img.onmouseout = function ()
+    {
+        this.src = "img/cat.png";
+    }
+}
