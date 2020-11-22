9de38b0e35d345c2bed138a7b3ad9ac819c483ad
HowFunLin <1003941051@qq.com>
2020-11-22 22:40:17 +0800

    完成无缝轮播设计
    Signed-off-by: HowFunLin <1003941051@qq.com>

---
+++ 实验三/tab.js
@@ -0,0 +1,8 @@
+jQuery(window).ready(function () {
+    $(".tab>li").mouseenter(function () {
+
+		$(this).addClass("active").siblings("li").removeClass("active");
+
+        $(".products>div").eq($(this).index()).addClass("selected").siblings("div").removeClass("selected");
+    });
+});
